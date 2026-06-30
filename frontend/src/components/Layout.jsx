24d6d0fba11b4ed92653d39251a054be0ac6030d
posamericanburger-maker <posamericanburger@gmail.com>
2026-06-29 22:22:28 -0400

    Update Layout component for full width main section

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ title, children }) => {
       <div className="min-h-screen flex flex-col lg:ml-64">
         {title && <Navbar title={title} />}
 
-        <main className="flex-1 overflow-x-hidden overflow-y-auto px-3 py-4 pt-20 pb-24 sm:px-5 md:px-6 lg:pt-6 lg:pb-6">
+        <main className="flex-1 w-full overflow-x-hidden px-3 py-4 pt-20 pb-24 sm:px-5 md:px-6 lg:pt-6 lg:pb-6">
           {children}
         </main>
       </div>
